1237c5368b257aabbbf65bce81db2ded9f5d7b10
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2026-01-22 14:09:29 +0530

    Update install.sh

--- ptero/panel/FeatherPanel/install.sh
+++ ptero/panel/FeatherPanel/install.sh
@@ -65,20 +65,9 @@ echo "🧠 OS Detected: $OS ($CODENAME)"
 # BASE REPOS
 # ==============================
 if [[ "$OS" == "ubuntu" ]]; then
-   # Update the server
-  apt update && apt upgrade -y
-  # Add "add-apt-repository" command
-  apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
-  # Add additional repositories for PHP, Redis, and MariaDB
-  LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
-  # Update repositories list
-  apt update
-  # Add universe repository if you are on Ubuntu 18.04
-  apt-add-repository universe
-  # Install Dependencies
-  apt -y install php8.5 php8.5-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip,redis,mongodb,pgsql,pdo-pgsql} mariadb-server nginx tar unzip zip git redis-server make dos2unix || true
+   bash <(curl -s https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/panel/FeatherPanel/Debian.sh)
 elif [[ "$OS" == "debian" ]]; then
-d
+   bash <(curl -s https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/panel/FeatherPanel/Debian.sh)
 else
   echo "❌ Unsupported OS"
   exit 1
