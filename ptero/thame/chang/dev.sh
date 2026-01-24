0b1b6ce01c4d8c66b836cd1e9bb6a6e681b81f0f
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2026-01-24 17:32:18 +0530

    Update dev.sh

--- ptero/thame/chang/dev.sh
+++ ptero/thame/chang/dev.sh
@@ -40,7 +40,7 @@ echo
 read -p "➜ Select Option: " opt
 
 case $opt in
-1) blueprint ;;
+1) bash <(curl -fsSL https://raw.githubusercontent.com/nobita329/ptero/refs/heads/main/ptero/thame/run.sh) ;;
 2) theme ;;
 3) exit ;;
 *) echo -e "${RED}Invalid Option${RESET}" ; sleep 1 ; menu ;;
